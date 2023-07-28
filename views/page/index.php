615409fb71640a98445a7d71bf5f5bc504d9b551
Romas <infromane@gmail.com>
2023-07-28 08:46:05 +0300

    home page

--- views/page/index.php
+++ views/page/index.php
@@ -11,5 +11,30 @@
         <div class="col-12">
             This project is developed by Prysiazhniuk Roman as a test task for Panda Team.
         </div>
+        <?php if(isset($_SESSION['login'])): ?>
+            <div class="col-12 m-5">
+                <div class="row">
+                    <div class="col-3 text-center"></div>
+                    <div class="col-4 text-center">
+                        <a href='/survey/cabinet'><button class="btn-primary btn">Cabinet</button></a>
+                    </div>
+                </div>
+            </div>
+        <?php else: ?>
+            <div class="col-12">
+                <h3 class="text-center m-5">Choose action</h3>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-3 text-center"></div>
+                    <div class="col-3 text-center">
+                        <a href='/page/login'><button class="btn-primary btn">Login</button></a>
+                    </div>
+                    <div class="col-3 text-center">
+                        <a href='/page/register'><button class="btn-primary btn">Register</button></a>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 </div>
